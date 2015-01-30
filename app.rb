9016a4c31d45b35157ba6dcbf0e45dc31977fc10
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-01-30 12:00:26 -0800

    add ability to submit new band

--- app.rb
+++ app.rb
@@ -8,3 +8,14 @@ also_reload 'lib/**/*.rb'
 get '/' do
   erb :index
 end
+
+get '/bands/new' do
+  erb :new_band
+end
+
+post '/bands' do
+  band_name = params.fetch("band_name")
+  @band = Band.new(band_name: band_name)
+  @band.save
+  erb :bands
+end
