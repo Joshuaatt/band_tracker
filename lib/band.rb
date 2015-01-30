5e3953cd1a9804668dd2be1e63adc1db86eef9a3
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-01-30 15:18:41 -0800

    validate and capitalize band and venue

--- lib/band.rb
+++ lib/band.rb
@@ -1,3 +1,12 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
+  validates(:band_name, :presence => true)
+  before_save(:capitalize_name)
+
+
+private
+
+  def capitalize_name
+    self.band_name = band_name.capitalize
+  end
 end
--- lib/venue.rb
+++ lib/venue.rb
@@ -1,3 +1,12 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many :bands
+  validates(:venue_name, :presence => true)
+  before_save(:capitalize_name)
+
+
+private
+
+  def capitalize_name
+    self.venue_name = venue_name.capitalize
+  end
 end
