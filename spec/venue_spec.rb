605b75e552634f5bcd8430c4cce7419215807e9a
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-02 15:50:08 -0800

    Add venue  name capitalize spec

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe Venue do
   it { should have_and_belong_to_many :bands}
   it { should validate_presence_of :venue_name}
+
+  it 'Capitalizes the venue name' do
+    test_venue = Venue.create(venue_name: 'stubbs')
+    expect(test_venue.venue_name).to eq 'Stubbs'
+  end
 end
