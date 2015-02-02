1a2f28d262811e0dd9c8aa4f5e29cf017827a6b4
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-02 15:47:34 -0800

    Add band name capitalize spec

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe Band do
   it { should have_and_belong_to_many :venues }
   it { should validate_presence_of :band_name}
+
+  it "capitalizes the band name" do
+    test_band = Band.create(band_name: 'bach')
+    expect(test_band.band_name).to eq 'Bach'
+  end
 end
